f4fa14cb9b1e1f49a5360722ba3be0a61f982ba0
이원형 <67450169+whl0526@users.noreply.github.com>
2024-02-20 13:40:16 +0900

    Initial Commit

---
+++ Here-Hear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Here-Hear
+//
+//  Created by 이원형 on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Here-HearUITests/Here_HearUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Here_HearUITestsLaunchTests.swift
+//  Here-HearUITests
+//
+//  Created by 이원형 on 2/20/24.
+//
+
+import XCTest
+
+final class Here_HearUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
