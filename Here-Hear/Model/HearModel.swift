a58c1f90b841ff1f7e238995c14bcc91ef9c756d
MartinLeeSJ <tjr2922@gmail.com>
2024-02-21 14:24:20 +0900

    fix: Model의 구조체 선언, 구조체 이름 뒤에 Model 접미사 붙였음

--- Here-Hear/Model/HearModel.swift
+++ Here-Hear/Model/HearModel.swift
@@ -1,8 +1,10 @@
 //
-//  MusicModel.swift
+//  HearModel.swift
 //  Here-Hear
 //
 //  Created by 이원형 on 2/20/24.
 //
 
 import Foundation
+
+struct HearModel {}
--- Here-Hear/Model/MusicModel.swift
+++ Here-Hear/Model/MusicModel.swift
@@ -1,8 +1,10 @@
 //
-//  FeelingModel.swift
+//  MusicModel.swift
 //  Here-Hear
 //
 //  Created by 이원형 on 2/20/24.
 //
 
 import Foundation
+
+struct MusicModel {}
