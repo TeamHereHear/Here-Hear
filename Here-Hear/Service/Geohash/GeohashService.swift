c42b5554f2acf1a664834633604756b40be0564c
이원형 <67450169+whl0526@users.noreply.github.com>
2024-02-27 15:46:56 +0900

    feat: 1. Geohash 정확도를 정의하는 파일 추가 2. 위치 데이터 처리를 위한 모델 도입 3. 감정, 소리 및 음악 엔티티를 위한 모의 데이터 파일 추가 4. 일부 파일에 Codable 프로토콜 추가 5. HearModel의 좌표 속성을 LocationModel로 교체 6. Combine 프레임워크 및 코드를 HearRepository에 통합 7. geohash 패키지 정보를 포함하는 Package.resolved 파일 추가 8. 프로젝트 및 파일 구조 재구성, Xcode 프로젝트 파일에 종속성 추가

---
+++ Here-Hear/Service/Geohash/GeohashService.swift
@@ -0,0 +1,51 @@
+//
+//  GeohashService.swift
+//  Here-Hear
+//
+//  Created by 이원형 on 2/27/24.
+//
+
+import Foundation
+import Geohash
+
+protocol GeohashServiceInterface {
+    func geohashExact(latitude: Double, longitude: Double) -> String
+    func overlappingGeohash(latitude: Double, longitude: Double, precision: Int) -> [String]
+    
+}
+
+class GeohashService: GeohashServiceInterface {
+    /// 기준점의 정확한 Geohash(12글자 길이)
+    /// - Parameters:
+    ///   - latitude: 기준점의 위도
+    ///   - longitude: 기준점의 경도
+    /// - Returns: 12글자 길이의 Geohash
+    func geohashExact(latitude: Double, longitude: Double) -> String {
+        Geohash.encode(latitude: latitude, longitude: longitude, length: 12)
+    }
+    
+    /// 기준점 반경에 걸쳐있는 Geohash를 구하는 메서드
+    /// - Parameters:
+    ///   - latitude: 기준점의 위도
+    ///   - longitude: 기준점의 경도
+    ///   - precision: Geohash 정확도(길이)
+    /// - Returns: 기준점 반경에 걸쳐있는 Geohash
+    func overlappingGeohash(latitude: Double, longitude: Double, precision: Int) -> [String] {
+        let geohash = Geohash.encode(latitude: latitude, longitude: longitude, length: precision)
+        let neighbors = Geohash.neighbors(geohash: geohash)
+        
+        let neighborsParents: [String] = neighbors.map { String($0.dropLast()) }
+        
+        return Array(Set(neighborsParents))
+    }
+}
+
+class StubGeohashService: GeohashServiceInterface {
+    func geohashExact(latitude: Double, longitude: Double) -> String {
+        // 37.566406, 126.977822
+        "wydm9qy2jtws"
+    }
+    func overlappingGeohash(latitude: Double, longitude: Double, precision: Int) -> [String] {
+        []
+    }
+}
