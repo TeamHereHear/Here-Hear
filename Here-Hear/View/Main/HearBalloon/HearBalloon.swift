e2c17c0be672b33f6f385be015ae77c61695eebe
MartinLeeSJ <tjr2922@gmail.com>
2024-03-13 21:54:21 +0900

    feat: HearBalloonViewModel

--- Here-Hear/View/Main/HearBalloon/HearBalloon.swift
+++ Here-Hear/View/Main/HearBalloon/HearBalloon.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct HearBalloon: View {
+    @StateObject private var viewModel: HearBalloonViewModel
+    
     private let width: CGFloat = 185
     private let height: CGFloat = 63
     private let cornerRadius: CGFloat = 10
     private let tipHeight: CGFloat = 20
     
     private let albumArtWidth: CGFloat = 63
+    
+    init(viewModel: HearBalloonViewModel) {
+        self._viewModel = StateObject(wrappedValue: viewModel)
+    }
     
     var body: some View {
         HStack(spacing: 0) {
@@ -35,11 +41,15 @@ struct HearBalloon: View {
                 tipHeight: tipHeight
             )
         }
+        .onAppear {
+            viewModel.fetchMusic()
+            viewModel.fetchHearUser()
+        }
     }
     
     private var albumArt: some View {
         RemoteImage(
-            path: "",
+            path: viewModel.music?.previewURL?.absoluteString,
             isStorageImage: false,
             transitionDuration: 1) { ProgressView() }
             .frame(
@@ -60,19 +70,24 @@ struct HearBalloon: View {
     
     @ViewBuilder
     private var musicInfo: some View {
-        Text("I Like you")
-            .font(.system(size: 16, weight: .semibold))
-            .foregroundStyle(.black)
+        Group {
+            Text(viewModel.music?.title ?? "Music Title")
+                .font(.system(size: 16, weight: .semibold))
+                .foregroundStyle(.black)
+            
+            Text(viewModel.music?.artist ?? "Artist")
+                .font(.system(size: 14, weight: .regular))
+                .foregroundStyle(.white)
+        }
+        .redacted(reason: viewModel.music == nil ? .placeholder : [])
         
-        Text("Post Malone")
-            .font(.system(size: 14, weight: .regular))
-            .foregroundStyle(.white)
     }
     
     private var hearInfo: some View {
         HStack(spacing: 0) {
-            Text("Wonhyeong")
+            Text(viewModel.userNickname ?? "Nickname")
                 .foregroundStyle(.white)
+                .redacted(reason: viewModel.userNickname == nil ? .placeholder : [])
             
             Spacer()
             
@@ -80,63 +95,9 @@ struct HearBalloon: View {
                 .foregroundStyle(.hhAccent2)
                 .padding(.trailing, 3)
             
-            Text(120, format: .number)
+            Text(viewModel.like, format: .number)
                 .foregroundStyle(.white)
         }
         .font(.system(size: 11, weight: .regular))
     }
-}
-
-private struct HearBalloonBackground: View {
-    private let width: CGFloat
-    private let height: CGFloat
-    private let cornerRadius: CGFloat
-    private let tipHeight: CGFloat
-    
-    init(
-        width: CGFloat,
-        height: CGFloat,
-        cornerRadius: CGFloat,
-        tipHeight: CGFloat
-    ) {
-        self.width = width
-        self.height = height
-        self.cornerRadius = cornerRadius
-        self.tipHeight = tipHeight
-    }
-    
-    var body: some View {
-        Path { path in
-            path.addRoundedRect(
-                in: .init(x: 0, y: 0, width: width, height: height),
-                cornerSize: .init(width: cornerRadius, height: cornerRadius),
-                style: .continuous
-            )
-            
-            path.move(to: .init(x: width / 2, y: height + tipHeight))
-            
-            path.addLine(
-                to: .init(
-                    x: (width / 2) - (tipHeight / 2),
-                    y: height
-                )
-            )
-            
-            path.addLine(
-                to: .init(
-                    x: (width / 2) + (tipHeight / 2),
-                    y: height
-                )
-            )
-            
-            path.closeSubpath()
-        }
-        .foregroundStyle(.hhSecondary)
-    }
-}
-
-#Preview {
-    HearBalloon()
-        .padding()
-        .environmentObject(DIContainer(services: StubServices()))
 }
---
+++ Here-Hear/View/Main/HearBalloon/HearBalloonViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  HearBalloonViewModel.swift
+//  Here-Hear
+//
+//  Created by Martin on 3/13/24.
+//
+
+import Foundation
+import Combine
+
+final class HearBalloonViewModel: ObservableObject {
+    @Published var music: MusicModel?
+    @Published var userNickname: String?
+    public var like: Int {
+        hear.like
+    }
+    
+    private let hear: HearModel
+    
+    private let container: DIContainer
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(hear: HearModel, container: DIContainer) {
+        self.hear = hear
+        self.container = container
+    }
+    
+    func fetchMusic() {
+        guard let musicId = hear.musicIds.first else { return }
+        
+        container.services.musicService.fetchMusic(ofIds: [musicId])
+            .sink { completion in
+                switch completion {
+                case .finished: 
+                    print("finished")
+                case .failure(let error): 
+                    print(error)
+                }
+            } receiveValue: { [weak self] models in
+                guard let self else { return }
+                Task {
+                    await self.setMusic(models.first)
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    @MainActor
+    private func setMusic(_ music: MusicModel?) {
+        self.music = music
+    }
+    
+    func fetchHearUser() {
+        container.services.userService.fetchUser(ofId: hear.userId)
+            .sink { _ in
+            } receiveValue: { [weak self] user in
+                guard let self else { return }
+                Task {
+                    await self.setUserNickname(user?.nickname)
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    @MainActor
+    private func setUserNickname(_ nickname: String?) {
+        self.userNickname = nickname
+    }
+    
+}
